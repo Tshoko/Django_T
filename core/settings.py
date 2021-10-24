37b2912d53b073223247166a81e9089bb92f2e11
Tshoko <40602097+Tshoko@users.noreply.github.com>
2021-10-24 11:58:49 +0200

    First chat app2/-/runtime engine fixed

--- core/settings.py
+++ core/settings.py
@@ -130,8 +130,15 @@ CHANNEL_LAYERS = {
 '''
 
 
-
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+'''
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
@@ -151,4 +158,4 @@ CACHES = {
     }
 }
 
-
+'''
